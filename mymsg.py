a05d721da6f247dc83d5f537642192fcde3ffcf5
Viktor <enotcode@gmail.com>
2017-11-13 04:22:18 +0300

    Add messages📝

---
+++ mymsg.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+messages = {
+    "ru": {
+        "welcome": "Добро пожаловать 🚀",
+        "add": "Добавить задание ✏️",
+        "mytask": "Мои задания 📝",
+        "wtask": "Напишите ваше задание:",
+        "utask": "Ваши задания:",
+        "help": "Помощь 📚",
+        "ref": "Если у вас возникли вопросы, пишите @enotcode"
+    },
+    "en": {
+        "welcome": "Welcome 🚀",
+        "add": "Add task ✏️",
+        "mytask": "My tasks 📝",
+        "wtask": "Write your task:",
+        "utask": "Ваши задания:",
+        "help": "Help 📚",
+        "ref": "If you have any questions, please contact @enotcode"
+    }
+}
