2a62bfd5d0cb54620d4c750d2c7d6bcf12469ddf
Viktor <enotcode@gmail.com>
2017-11-17 00:01:25 +0300

    Add admin id ✏️

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 token = os.environ.get("TOKEN", 5000)
 base = os.environ.get("BASE", 5000)
 botan_key = os.environ.get("BOTAN", 5000)
+adminID = os.environ.get("ADMIN", 5000)
 url = "https://itodobot.herokuapp.com/bot"
