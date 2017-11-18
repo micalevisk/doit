4d1dc7c0341cf736966384b5fd653537cc253f6b
Viktor <enotcode@gmail.com>
2017-11-18 19:12:47 +0300

    fix url environment🔥

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ token = os.environ.get("TOKEN", 5000)
 base = os.environ.get("BASE", 5000)
 botan_key = os.environ.get("BOTAN", 5000)
 adminID = os.environ.get("ADMIN", 5000)
-url = "https://itodobot.herokuapp.com/bot"
+url = os.environ.get("URL", 5000)
