f48ea92b93c159c4d1eb7349080bd138c23434ab
Viktor <enotcode@gmail.com>
2017-11-25 02:34:09 +0300

    fix 🔥

--- helper.py
+++ helper.py
@@ -22,7 +22,6 @@ def gen_markup(add_task, my_task, my_help, rate, notify):
 
 def tasks_kb(tasks):
     keyboard = types.InlineKeyboardMarkup()
-    tasks.reverse()
     for i in range(len(tasks)):
         button = types.InlineKeyboardButton(text=tasks[i], callback_data=str(i))
         keyboard.add(button)
