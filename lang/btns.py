a22a5d129bd0ef9a75512e9553651d62d5298a56
Viktor <enotcode@gmail.com>
2018-01-10 15:34:03 +0300

    Add languages 📚

---
+++ lang/btns.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+add = ["Add task \u270f\ufe0f", "Добавить задачу \u270f\ufe0f"]
+task = ["My tasks \U0001f4dd", "Мои задачи \U0001f4dd"]
+help = ["Help \U0001f4da", "Помощь \U0001f4da"]
+rate = ["Rate \u2b50\ufe0f", "Оценить \u2b50\ufe0f"]
+off = ["Off notifications \U0001f515", "Выключить уведомления \U0001f515"]
+on = ["On notifications \U0001f514", "Включить уведомления \U0001f514"]
+setl = ["Set language \U0001f6e0", "Выбрать язык \U0001f6e0"]
+cancel = ["Cancel \u274c", "Отмена \u274c"]
