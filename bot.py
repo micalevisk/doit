cf46fe8937519f0c285335b1adae55ccb8f7d992
Viktor <enotcode@gmail.com>
2018-01-15 00:18:53 +0300

    Update 🚀

--- bot.py
+++ bot.py
@@ -25,7 +25,6 @@ botan_key = config.botan_key
 client = MongoClient(config.base)
 db = client["todo"]
 
-isWrite = False
 i18n.load_path.append("lang/")
 
 langs = {"Русский \U0001f1f7\U0001f1fa": "ru", "English \U0001f1fa\U0001f1f8": "en"}
@@ -45,20 +44,20 @@ def start(msg):
         else:
             markup = gen_markup(i18n.t("msg.add"), i18n.t("msg.tasks"), i18n.t("msg.help"), i18n.t("msg.rate"), i18n.t("msg.onn"), i18n.t("msg.setl"))
         bot.send_message(msg.chat.id, i18n.t("msg.hi_back"), reply_markup=markup)
+
         botan.track(botan_key, msg.chat.id, msg, 'Returned user')
         return
     else:
         markup = gen_markup(i18n.t("msg.add"), i18n.t("msg.tasks"), i18n.t("msg.help"), i18n.t("msg.rate"), i18n.t("msg.offn"), i18n.t("msg.setl"))
         db.users.save({"id": str(msg.chat.id), "tasks": [], "notify": True, "lang": "en"})
         bot.send_message(msg.chat.id, i18n.t("msg.hi"),reply_markup=markup)
+
         botan.track(botan_key, msg.chat.id, msg, 'New user')
         return
 
 
 @bot.message_handler(commands=["add"])
 def add(msg):
-    global isWrite
-    isWrite = True
     find = db.users.find_one({"id": str(msg.chat.id)})
     i18n.set("locale", find["lang"])
 
@@ -67,27 +66,31 @@ def add(msg):
     markup.add(i18n.t("msg.cancel"))
     if m == "":
         bot.send_message(msg.chat.id, i18n.t("msg.enter"), reply_markup=markup)
+        bot.register_next_step_handler(msg, add_hand)
     else:
         save_task(msg, msg.chat.id, m)
+
+    botan.track(botan_key, msg.chat.id, msg, 'Add task')
+    return
 
 
 @bot.message_handler(func=lambda msg: msg.text in btns.add)
 def add_task(msg):
-    global isWrite
-    isWrite = True
     find = db.users.find_one({"id": str(msg.chat.id)})
     i18n.set("locale", find["lang"])
 
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     markup.add(i18n.t("msg.cancel"))
     bot.send_message(msg.chat.id, i18n.t("msg.enter"), reply_markup=markup)
+    bot.register_next_step_handler(msg, add_hand)
+
+    botan.track(botan_key, msg.chat.id, msg, 'Add task')
+    return
 
 
 @bot.message_handler(commands=["tasks"])
 @bot.message_handler(func=lambda msg: msg.text in btns.task)
 def my_task(msg):
-    global isWrite
-    isWrite = False
     find = db.users.find_one({"id": str(msg.chat.id)})
     i18n.set("locale", find["lang"])
 
@@ -101,8 +104,6 @@ def my_task(msg):
 
 @bot.message_handler(func=lambda msg: msg.text in btns.cancel)
 def cancel(msg):
-    global isWrite
-    isWrite = False
     find = db.users.find_one({"id": str(msg.chat.id)})
     i18n.set("locale", find["lang"])
 
@@ -125,6 +126,7 @@ def rate(msg):
         text=i18n.t("msg.rate"), url="https://t.me/storebot?start=jditbot")
     kb.add(btn)
     bot.send_message(msg.chat.id, i18n.t("msg.thanks"), reply_markup=kb)
+
     botan.track(botan_key, msg.chat.id, msg, 'Rate')
     return
 
@@ -134,8 +136,8 @@ def rate(msg):
 def help(msg):
     find = db.users.find_one({"id": str(msg.chat.id)})
     i18n.set("locale", find["lang"])
-
     bot.send_message(msg.chat.id, i18n.t("msg.helpme"))
+
     botan.track(botan_key, msg.chat.id, msg, 'Help')
     return
 
@@ -146,6 +148,7 @@ def lang(msg):
     find = db.users.find_one({"id": str(msg.chat.id)})
     i18n.set("locale", find["lang"])
     bot.send_message(msg.chat.id, i18n.t("msg.setlang"), reply_markup=lang_markup)
+    bot.register_next_step_handler(msg, lang_hand)
 
 
 @bot.message_handler(func=lambda msg: msg.text in btns.off or msg.text in btns.on)
@@ -163,17 +166,6 @@ def notifyset(msg):
         bot.send_message(msg.chat.id, i18n.t("msg.enabled"), reply_markup=markup)
 
 
-@bot.message_handler(func=lambda msg: True)
-def msg_hand(msg):
-    global isWrite
-    if isWrite and msg.text not in langs:
-        save_task(msg, msg.chat.id, msg.text)
-    elif msg.text in langs:
-        find = db.users.find_one({"id": str(msg.chat.id)})
-        db.users.update({"id": str(msg.chat.id)}, {"$set": {"lang": langs.get(msg.text)}})
-        i18n.set("locale", langs.get(msg.text))
-        bot.send_message(msg.chat.id, i18n.t("msg.newlang"))
-    isWrite = False
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -195,7 +187,6 @@ def callback_inline(call):
 
 
 def save_task(msg, cid, text):
-    global isWrite
     find = db.users.find_one({"id": str(cid)})
     i18n.set("locale", find["lang"])
 
@@ -214,7 +205,19 @@ def save_task(msg, cid, text):
             bot.send_message(cid, i18n.t("msg.inlist"), reply_markup=markup)
     else:
         bot.send_message(cid, i18n.t("msg.long"), reply_markup=markup)
-    isWrite = False
+
+
+def add_hand(msg):
+    if msg.text not in langs and msg.text not in btns.cancel:
+        save_task(msg, msg.chat.id, msg.text)
+
+
+def lang_hand(msg):
+    if msg.text in langs:
+        find = db.users.find_one({"id": str(msg.chat.id)})
+        db.users.update({"id": str(msg.chat.id)}, {"$set": {"lang": langs.get(msg.text)}})
+        i18n.set("locale", langs.get(msg.text))
+        bot.send_message(msg.chat.id, i18n.t("msg.newlang"))
 
 
 @cron.interval_schedule(hours=12)
